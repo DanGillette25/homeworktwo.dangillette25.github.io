4cb4cf3af86c208df8605632c0ec99ca093beada
Dan Gillette <daniel.gillette87@gmail.com>
2020-10-04 20:47:55 -0700

    Updating portfolio with latest and greatest

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 const modal = document.getElementById("myModal");
 
 const descriptions = [
-    {name: "Password Generator",
+    {name: "EmployeeDB (Runs in Terminal)",
      repoLink: "https://github.com/DanGillette25/homeworkthree.dangillette25.github.io",
      deployed: "https://dangillette25.github.io/homeworkthree.dangillette25.github.io/"
     },
@@ -13,9 +13,9 @@ const descriptions = [
      deployed: "https://dangillette25.github.io/homeworkfour.dangillette25.github.io/?"
     },
     {
-    name: "Day Planner",
-    repoLink: "https://github.com/DanGillette25/homeworkfive.dangillette25.github.io",
-    deployed: "https://dangillette25.github.io/homeworkfive.dangillette25.github.io/",
+    name: "BreakAway",
+    repoLink: "https://github.com/MVC-5/breakaway",
+    deployed: "https://breakaway-vacay.herokuapp.com/",
     },
     {
      name: "Weather Dashboard",
@@ -27,7 +27,15 @@ const descriptions = [
     name: "DiscoverSerenity",
     repoLink: "https://github.com/calebkirkish/discover-serenity",
     deployed: "https://calebkirkish.github.io/discover-serenity/"
-    }
+    },
+
+    {
+    name: "Note Taker",
+    repoLink: "https://github.com/DanGillette25/notetaker",
+    deployed: "https://dg-notetaker-app.herokuapp.com"
+    },
+
+
 ]
 
 
@@ -43,8 +51,35 @@ const descriptions = [
         $("#app-title").text(descriptions[descriptionNumber].name);
         $("#repo").attr('href', descriptions[descriptionNumber].repoLink)
         $("#repo").attr('target', "_blank")
+        $("#deploy").css('display', 'block')
         $("#deploy").attr('href', descriptions[descriptionNumber].deployed)
         $("#deploy").attr('target', '_blank')
+
+
+
+        $(".close").on("click", function(){
+
+            modal.style.display = "none";
+
+        })
+
+        
+
+    
+    })
+
+    $(".nodepic").on("click", function(){
+        
+        
+        let descriptionNumber = parseInt($(this).attr('value'));
+        
+
+        modal.style.display = "block";
+
+        $("#app-title").text(descriptions[descriptionNumber].name);
+        $("#repo").attr('href', descriptions[descriptionNumber].repoLink)
+        $("#repo").attr('target', "_blank")
+        $("#deploy").css('display', 'none')
 
 
 
